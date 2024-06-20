f415cba01007f8ab5a7143f82d1ced1c5e1b0130
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-06-20 16:47:21 +0200

    Second Commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,11 +3,11 @@ export default function Button ({title, onClick, activeTab}) {
     return (
         <>
                 <div
-                    className={`inline-block outline-none ring-white rounded border border-neutral-600 dark:border-white px-12 py-3 text-sm font-medium text-neutral-600 dark:text-white hover:bg-neutral-600 hover:text-white cursor-pointer focus:outline-none 
+                    className={`inline-block outline-none ring-white rounded border border-neutral-600 dark:border-white px-12 py-3 text-sm font-medium text-neutral-600  cursor-pointer focus:outline-none 
                         ${
                             activeTab === title 
-                            ? "bg-neutral-600 text-white dark:bg-white dark:text-gray-700" 
-                            : "text-neutral-500 dark:text-white"
+                            ? "bg-neutral-600 dark:text-black text-white dark:bg-white "
+                            : "hover:bg-neutral-600 hover:text-white dark:text-white" 
                         }`}
                     onClick={onClick}
                 >
